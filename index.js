0399a89dc5a97272ba34f2c087cd00e1d2b4653c
Jar'Zeno <jarzenojarrett@gmail.com>
2023-03-21 08:22:31 -0700

    Remove imports

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 const express = require("express");
-const logger = require("morgan");
-const routes = require("./src/scripts/routes");
-const square = require("./src/scripts/square");
 const app = express();
 const port = 8000;
 
-// app.use(routes);
 app.use(express.static("./src/html"));
-// app.get("/", (req, res) => {
-//     res.send(`Hello World! The area of a square with a width of 5 is ${square.area(5)}`);
-// });
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`);
